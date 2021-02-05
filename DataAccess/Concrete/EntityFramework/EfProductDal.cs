eccfa976f062fc44920bc29807b90e5344b77036
fatihsahin3 <fatihsahin3@gmail.com>
2021-02-05 23:59:05 +0100

    05.02.2021 Update

---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,63 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            //IDisposable pattern implementation of C#
+            using (NorthwindContext context = new NorthwindContext()) // using makes Garbage collector terminate this NorthwindContext object immediately when exiting using block.
+            {
+                var addedEntity = context.Entry(entity); // Get the reference of the object.
+                addedEntity.State = EntityState.Added; // State of reference will be set to "add".
+                context.SaveChanges(); // Save the context object.
+            }
+        }
+
+        public void Delete(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext()) // using makes Garbage collector terminate this NorthwindContext object immediately when exiting using block.
+            {
+                var deletedEntity = context.Entry(entity); // Get the reference of the object.
+                deletedEntity.State = EntityState.Deleted; // State of reference will be set to "add".
+                context.SaveChanges(); // Save the context object.
+            }
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return context.Set<Product>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return filter == null 
+                    ? context.Set<Product>().ToList() 
+                    : context.Set<Product>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext()) // using makes Garbage collector terminate this NorthwindContext object immediately when exiting using block.
+            {
+                var updatedEntity = context.Entry(entity); // Get the reference of the object.
+                updatedEntity.State = EntityState.Modified; // State of reference will be set to "add".
+                context.SaveChanges(); // Save the context object.
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context: Connection of database tables with project classes.
+    class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; Database=Northwind; Trusted_Connection=true"); // Defining that we will use SQL Server.
+        }
+
+        public DbSet<Product> Products { get; set; } // Here we define which DB Table element correspond to which class in the project.
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+    }
+}
